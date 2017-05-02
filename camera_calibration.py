b36bbca5cecaecaae44bea4b290bc996bb5c4228
Matthias Fuchs <matz.fuchs@gmx.at>
2017-05-02 22:20:46 +0200

    Calibrate camera

--- camera_calibration.py
+++ camera_calibration.py
@@ -1,58 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import pickle
+#import pickle
 import cv2
+import glob
 import numpy as np
+
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+#import matplotlib.image as mpimg
 
-# Read in the saved objpoints and imgpoints
-dist_pickle = pickle.load( open( "wide_dist_pickle.p", "rb" ) )
-objpoints = dist_pickle["objpoints"]
-imgpoints = dist_pickle["imgpoints"]
-
-# prepare object points
-nx = 8
+nx = 9
 ny = 6
 
-# Make a list of calibration images
-fname = 'calibration_test.png'
-img = cv2.imread(fname)
+objp = np.zeros((nx*ny, 3), np.float32)
+objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1, 2)
 
-# Convert to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+objpoints = []
+imgpoints = []
+for fname in glob.glob('camera_cal/calibration*.jpg'):
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+    if ret == True:
+        imgpoints.append(corners)
+        objpoints.append(objp)
 
-# Find the chessboard corners
-ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
 
-# If found, draw corners
-if ret == True:
-    # Draw and display the corners
-    cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
-    plt.imshow(img)
-    
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+print(ret, mtx, dist)
 
-# Read in an image
-img = cv2.imread('test_image.png')
-
-# TODO: Write a function that takes an image, object points, and image points
-# performs the camera calibration, image distortion correction and 
-# returns the undistorted image
-def cal_undistort(img, objpoints, imgpoints):
-    # Use cv2.calibrateCamera() and cv2.undistort()
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    undist = cv2.undistort(img, mtx, dist, None, mtx)
-    
-    return undist
-
-undistorted = cal_undistort(img, objpoints, imgpoints)
+img = cv2.imread('camera_cal/calibration1.jpg')
+undist = cv2.undistort(img, mtx, dist, None, mtx)
 
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
 ax1.imshow(img)
 ax1.set_title('Original Image', fontsize=50)
-ax2.imshow(undistorted)
+ax2.imshow(undist)
 ax2.set_title('Undistorted Image', fontsize=50)
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
